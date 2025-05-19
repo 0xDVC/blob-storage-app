c9cc73b72a6e0a85f677934d8a0a2f0045f11fbe
0xDVC <neilohene@gmail.com>
2025-05-19 14:09:20 +0000

    starter template

---
+++ src/test/java/com/upskill/file_storage_app/FileStorageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upskill.file_storage_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStorageAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
